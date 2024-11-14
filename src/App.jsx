7dcc228809bf2b7fa71deec3bbf207f7b1154012
Rahmatullah311 <714mohammadi@gmail.com>
2024-11-14 16:27:26 +0430

    skeleton preloader added to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const SolutionUrl = lazy(() => import("./solution/SolutionUrl"));
 const HomeUrls = lazy(() => import("./home/HomeUrls"));
+import SolutionsLoader from "./solution/loaders/SolutionsLoader";
 
 function App() {
   return (
     <>
       <Router>
-        <Suspense fallback={<p>Loading ...</p>}>
+        <Suspense fallback={<SolutionsLoader/>}>
           <Routes>
             <Route path="home/*" element={<HomeUrls />}></Route>
             <Route path="/*" element={<SolutionUrl />} />
